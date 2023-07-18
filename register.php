51628e3a216b6273a80e964999e897cc62c994b4
onwuobinna <112879625+onwuobinna@users.noreply.github.com>
2023-07-18 04:48:28 -0400

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>CSC 309 DB Class</title>
 </head>
+
 <body>
     <h2>Registration Form</h2>
 
@@ -22,7 +24,22 @@
             <input type="email" name="email">
         </div>
 
+        <div>
+            <label for="dob">Date of Birth</label>
+            <input type="date" name="dob">
+        </div>
+
+        <div>
+            <label for="gender">Gender</label>
+            <select name="gender">
+                <option>select gender</option>
+                <option>M</option>
+                <option>F</option>
+            </select>
+        </div>
+
         <button type="submit">Submit</button>
     </form>
 </body>
+
 </html>
